176d84853cb190471b797811ab2c10372952233a
hagay kohelet <hagay2506@gmail.com>
2025-11-26 12:01:46 +0200

    residence class

--- classes/residence.py
+++ classes/residence.py
@@ -1,4 +1,12 @@
-from pydantic import BaseModel
+class Residence:
+ def __init__(self):
+   self.rooms = 10
+   self.people = []
 
-class residence:
- pass
+ def check_rooms(self):
+  if len(self.people) == 8:
+   self.rooms -= 1
+   return False
+  return True
+
+
