8432d182d8b3a89e08ca432947644ba3ba65d456
hagay kohelet <hagay2506@gmail.com>
2025-11-26 12:05:48 +0200

    fix file indentation

--- classes/residence.py
+++ classes/residence.py
@@ -1,12 +1,12 @@
 class Residence:
- def __init__(self):
-   self.rooms = 10
-   self.people = []
+    def __init__(self):
+        self.rooms = 10
+        self.people = []
 
- def check_rooms(self):
-  if len(self.people) == 8:
-   self.rooms -= 1
-   return False
-  return True
+    def check_people_in_rooms(self):
+        if len(self.people) == 8:
+            self.rooms -= 1
+            return False
+        return True
 
 
